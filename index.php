872280942bdc7682cc2e2d34912b7c547084626a
freyman1985 <freyman1985@gmail.com>
2017-03-10 18:08:30 -0300

    first

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Sigma</title>
+    <!-- Bootstrap -->
+     <link href="css/estilos.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <header>
+  <section id="header">
+  <div class="navbar-wrapper">
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img class="first-slide" src="imagenes/logo.png" alt="First slide"></a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">       
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Inicio<span class="sr-only">(current)</span></a></li>
+              <li class="texto-menu"><a href="">Quienes Somos</a></li>
+              <li class="texto-menu"><a href="">Nuestros Servicios</a></li>
+               <li class="texto-menu"><a href="">Trabaje con Nosotros</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->      
+      </nav>
+    </div> <!--/.navbar-wrapper -->
+    </section>
+</header>
+<!-- Carousel
+    ================================================== -->
+    <section id="carrusel">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="imagenes/home_1.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h2>15 años de experiencia en el sector Minero-Industrial</h2>
+              <p><a class="btn btn-lg btn-warning" href="#" role="button">Nuestra Experiencia</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="imagenes/home_2.png" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h2>Empresa Certificada ISO 9001:2008</h2>
+              <h2>ISO 14001:2007</h2>
+              <h2>OHSAS 18001:2004</h2>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="imagenes/home_3.png" alt="Third slide">
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+    </section>
+ <section id="Nosotros">
+    <div class="container-fluid">
+      <div class="col-md-7">
+        <div class="col-md-12 text-center"><h2 class="marco">Quienes Somos</h2></div>
+        <div class="col-md-12 text-justify fondo-gris">      
+          SIGMA S.A. está formada por un amplio equipo, integrado por prestigiosos profesionales del área minera, civil, industrial, legal y de otras especialidades como el aseguramiento de la calidad, seguridad y salud ocupacional y, cuidado del medioambiente.
+
+Estos profesionales cuentan con una vasta experiencia en el campo del desarrollo de proyectos de servicios del rubro minero, civil, industrial, construcción y geomensura.
+        </div>
+        <div class="col-md-1 text-center botton-mas"><h2>+</h2></div>
+      </div>
+      <div class="col-md-5" style="height: 300px">
+        <img class="third-slide" src="imagenes/proyectos.jpg" style="height: 300px">
+      </div>
+    </div>
+
+ </section>
+
+     <section id="servicios">
+            <div id="services" class="pages">
+
+                <div class="container space-y-t">
+
+                    <div class="row">
+
+                        <div class="col-md-12 text-center">
+
+                            <h2 class="section-header">Nuestros Servicios <span class="underline animated" data-animation="fullWidth"></span></h2>
+
+                        </div>
+                        <!-- / col-md-12 -->
+
+                    </div>
+                    <!-- / row -->
+
+                </div>
+                <!-- / container -->
+                
+                <div class="container-fluid space-y-b-h">
+
+                    <div class="row">
+
+                        <div class="col-md-4 content-box-side animated" data-animation="fadeIn">
+
+                            <div class="box-side-details">
+                                <div class="box-side__header"> <i class="col-xs-offset-5"> <img class="" src="http://www.sigmasa.cl/files/icons/service-small.png"></i>
+                                    <h4 class="standard-title text-uppercase">Servicios Profesionales</h4> </div>
+                                <p>Actualmente las grandes empresas, en busca de una mayor eficiencia y calidad de sus procesos, los conduce a externalizar ...</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 content-box-side animated" data-animation="fadeIn" data-animation-delay="200">
+
+                            <div class="box-side-details">
+                                <div class="box-side__header"><i class="col-xs-offset-5"> <img class="" src="http://www.sigmasa.cl/files/icons/service-small.png"></i>
+                                    <h4 class="standard-title text-uppercase">Servicios de Construcción </h4> </div>
+                                <p>SIGMA S.A. y su Gerencia entregan un eficaz apoyo desde su centro de operaciones y un constante monitoreo ...</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 content-box-side animated" data-animation="fadeIn" data-animation-delay="400">
+
+                            <div class="box-side-details"><i class="col-xs-offset-5"> <img class="" src="http://www.sigmasa.cl/files/icons/service-small.png"></i><i class="fa fa-support"></i>
+                                    <h4 class="standard-title text-uppercase">Free support &amp; update</h4> </div>
+                                <p>Servicios de Mantención, con el fin de realizar una entrega total es que SIGMA S.A. entrega un servicio de Mantención ...</p>
+                            </div>
+                        </div>
+                        <!-- end row -->
+                    </div>
+                    <!-- end container -->
+                </div>
+                <!-- end servicios -->
+            </section>
+
+              <section id="valores">
+                <div class="infograph-container space-y">
+                    <div class="container">
+
+                        <div class="row">
+
+                            <div class="col-md-12">
+
+                                <div class="bubble">
+
+                                    <h4 class="bubble-center">
+                                    <span>Nuestros Valores</span>
+                                    
+                                </h4>
+
+                                    <ul>
+                                        <li class="animated vida" data-animation="bouncIn"><span>Respeto por la vida</span></li>
+                                        <li class="animated integridad" data-animation="bouncIn"><span>Integridad</span></li>
+                                        <li class="animated confianza" data-animation="bouncIn"><span>Confianza</span></li>
+                                        <li class="animated coraje" data-animation="bouncIn"><span>Coraje</span></li>
+                                        <li class="animated compromiso" data-animation="bouncIn"><span>Compromiso</span></li>
+                                        <li class="animated equipo" data-animation="bouncIn"><span>Trabajo en equipo</span></li>
+                                        <li class="animated excelencia" data-animation="bouncIn"><span>Excelencia</span></li>
+                                        
+                                    </ul>
+
+                                </div>
+                                <!-- / bubble -->
+
+                            </div>
+                            <!-- / col-md-12 -->
+
+                        </div>
+                        <!-- / row -->
+
+                    </div>
+                    <!-- / container -->
+
+                </div>
+                </section>
+                <!-- / infograph-container -->
+
+                <section id="clientes">
+                <div class="container space-y-t">
+
+                    <div class="row">
+
+                        <div class="col-md-12 text-center">
+
+                            <h2 class="section-header">Nuestros Clientes <span class="underline animated" data-animation="fullWidth"></span></h2>
+
+                        </div>
+                        <!-- / col-md-12 -->
+
+                    </div>
+                    <!-- / row -->
+
+                </div>
+                  <div class="row-fluid">
+                      <div class="col-md-3">
+                      <img class="first-slide" src="imagenes/arauco.png" alt="First slide">
+                      </div>
+                      <div class="col-md-3">
+                        <img class="first-slide" src="imagenes/codelco.png" alt="First slide">
+                      </div>
+                      <div class="col-md-3">
+                        <img class="first-slide" src="imagenes/esval.png" alt="First slide">
+                     </div>
+                      <div class="col-md-3">
+                        <img class="first-slide" src="imagenes/exxon.png" alt="First slide">
+                     </div>
+                    
+                  </div>
+                  
+                </section>
+
+                 <section id="footer">
+                      <div class="col-md-4" style="background: #eee;height: 300px">
+                        <div class="col-md-6" style="background: #aaa;height: 170px"></div>
+                        <div class="col-md-6" style="background: #ddd;height: 170px"></div>
+                        <div class="col-md-12" style="background: #000;height: 130px"></div>
+                      </div>
+                      <div class="col-md-4" style="background: #bbb;height: 300px">
+                        <h4>Oficina Central</h4>
+        <address  style="background: #eee;padding: 10px;">
+          <strong>Encuéntranos</strong><br>
+          Dirección: Diagonal Oriente #5170, Ñuñoa, Santiago<br>
+          Teléfono: (+56 2) 2963-6400<br>
+          Email: contacto@sigmasa.cl<br>      
+        </address>
+        <h4>Oficina Norte</h4>
+        <address  style="background: #eee;padding: 10px;">
+          <strong>Encuéntranos</strong><br>
+          Dirección: Diagonal Oriente #5170, Ñuñoa, Santiago<br>
+          Teléfono: (+56 2) 2963-6400<br>
+          Email: contacto@sigmasa.cl<br>      
+        </address>
+                     </div>
+                      <div class="col-md-4" style="background: #ddd;height: 300px">
+                      <div style="background: #eee;margin:20px;">
+                         <iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6657.503138051944!2d-70.578041!3d-33.45577839999999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDI3JzIwLjciUyA3MMKwMzQnNDEuNCJX!5e0!3m2!1ses-419!2scl!4v1429798511018"></iframe>
+                     </div>
+
+                     </div>
+                </section>
+    </div>
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
